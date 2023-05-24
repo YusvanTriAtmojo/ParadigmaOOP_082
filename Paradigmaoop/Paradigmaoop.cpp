15548a9f35a1c8a664dd902f677dff8d37de718a
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2023-05-24 13:56:43 +0700

    Membuat konstruktor dari class pelajar

--- Paradigmaoop/Paradigmaoop.cpp
+++ Paradigmaoop/Paradigmaoop.cpp
@@ -20,5 +20,10 @@ public:
 class pelajar : public orang {
 public:
 	string sekolah;
+	pelajar(string pNama, string pSekolah) :
+		orang(pNama),
+		sekolah(pSekolah) {
+		cout << "pelajar dibuat\n" << endl;
+	}
 };
 
