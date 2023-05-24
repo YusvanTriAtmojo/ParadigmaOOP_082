4bd90bef6687df3f76fcf2ecb8a9b53d4def60f1
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2023-05-24 14:15:35 +0700

    Membuat konstruktor dan destruktor dari class dokter dengan access public

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -21,4 +21,10 @@ class dokter {
 public:
 	string nama;
 	vector<pasien*> daftar_pasien;
+	dokter(string pNama) :nama(pNama) {
+		cout << "Dokter \"" << nama << "\" ada\n";
+	}
+	~dokter() {
+		cout << "Dokter \"" << nama << "\" tidak ada\n";
+	}
 };
